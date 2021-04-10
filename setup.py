db66ac3e0711626076e320f64c455eee2dce04c7
Jake <mapattacker@gmail.com>
2021-04-10 18:10:58 +0800

    Fix package typo

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from distutils.core import setup
 
 setup(
-  name = 'dendrogram_ts',         # How you named your package folder (MyLib)
-  packages = ['dendrogram_ts'],   # Chose the same as "name"
-  version = '0.1.0-beta.2',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Plotting time-series graphs in scipy''s dendrogram',   # Give a short description about your library
-  author = 'Jake Teo',                   # Type in your name
-  author_email = 'mapattacker@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/mapattacker/dendrogram-ts',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mapattacker/dendrogram-ts/archive/refs/tags/v0.1.0-beta.2.tar.gz',    # I explain this later on
-  keywords = ['dendrogram', 'agglomerative clustering', 'timeseries'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-            'pandas==1.2.3'
-            'numpy==1.20.2',
-            'matplotlib==3.4.1',
-            'scipy==1.6.2',
+  name = 'dendrogram_ts',
+  packages = ['dendrogram_ts'],
+  version = '0.1.0-beta.2',    
+  license='MIT',
+  description = "Plotting time-series graphs in scipy's dendrogram",
+  author = 'Jake Teo',
+  author_email = 'mapattacker@gmail.com',
+  url = 'https://github.com/mapattacker/dendrogram-ts',
+  download_url = 'https://github.com/mapattacker/dendrogram-ts/archive/refs/tags/v0.1.0-beta.2.tar.gz',
+  keywords = ['dendrogram', 'agglomerative clustering', 'timeseries'],
+  install_requires=[
+            'pandas==1.2.*',
+            'numpy==1.20.*',
+            'matplotlib==3.4.*',
+            'scipy==1.6.*',
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.6',    # Specify which python versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8'
