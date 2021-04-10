c8f1def1382ee9571776cd7daaad4073f39f0bd1
Jake <mapattacker@gmail.com>
2021-04-10 18:30:44 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 
-with open('README.md') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 setup(
   name = 'dendrogram_ts',
   packages = ['dendrogram_ts'],
-  version = '0.1.0-beta.3',    
+  version = '0.1.0-beta.4',    
   license='MIT',
   description = "Plotting time-series graphs in scipy's dendrogram",
   long_description = readme,
   author = 'Jake Teo',
   author_email = 'mapattacker@gmail.com',
   url = 'https://github.com/mapattacker/dendrogram-ts',
-  download_url = 'https://github.com/mapattacker/dendrogram-ts/archive/refs/tags/v0.1.0-beta.3.tar.gz',
+  download_url = 'https://github.com/mapattacker/dendrogram-ts/archive/refs/tags/v0.1.0-beta.4.tar.gz',
   keywords = ['dendrogram', 'agglomerative clustering', 'timeseries'],
   install_requires=[
             'pandas==1.2.*',
