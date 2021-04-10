a940627dc552c810bd22ce60b15ce28e8df33f0d
Jake <mapattacker@gmail.com>
2021-04-10 18:03:44 +0800

    edits for pypi release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
   name = 'dendrogram_ts',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1.0-beta',      # Start with a small number and increase it with every change you make
+  packages = ['dendrogram_ts'],   # Chose the same as "name"
+  version = '0.1.0-beta.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Plotting time-series graphs in scipy''s dendrogram',   # Give a short description about your library
   author = 'Jake Teo',                   # Type in your name
   author_email = 'mapattacker@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/mapattacker/dendrogram-ts',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mapattacker/dendrogram-ts/archive/refs/tags/v0.1.0-beta.2.tar.gz',    # I explain this later on
   keywords = ['dendrogram', 'agglomerative clustering', 'timeseries'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
             'pandas==1.2.3'
