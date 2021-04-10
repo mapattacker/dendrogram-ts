31553536cb120f956109d0bf6a2d766796fc6eb5
Jake <mapattacker@gmail.com>
2021-04-10 21:09:53 +0800

    change version

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst') as readme_file:
 setup(
   name = 'dendrogram_ts',
   packages = ['dendrogram_ts'],
-  version = '0.1.0-beta.6',
+  version = '0.1.0-beta.7',
   license='MIT',
   description = "Plotting time-series graphs in scipy's dendrogram",
   long_description = readme,
   author = 'Jake Teo',
   author_email = 'mapattacker@gmail.com',
   url = 'https://github.com/mapattacker/dendrogram-ts',
-  download_url = 'https://github.com/mapattacker/dendrogram-ts/archive/refs/tags/v0.1.0-beta.6.tar.gz',
+  download_url = 'https://github.com/mapattacker/dendrogram-ts/archive/refs/tags/v0.1.0-beta.7.tar.gz',
   keywords = ['dendrogram', 'agglomerative clustering', 'timeseries'],
   install_requires=[
             'pandas==1.2.*',
